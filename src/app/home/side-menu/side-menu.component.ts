1b38f02054126f9a22829f2893edcf9e1ce21644
bpa5758 <bpa182@gmail.com>
2023-12-27 16:55:11 +0700

    refactor: add side bar offcanvas

--- src/app/home/side-menu/side-menu.component.ts
+++ src/app/home/side-menu/side-menu.component.ts
@@ -4,11 +4,12 @@ import {
   NgbAccordionModule,
   NgbOffcanvasModule,
 } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [CommonModule, NgbAccordionModule, NgbOffcanvasModule],
+  imports: [CommonModule, NgbAccordionModule, NgbOffcanvasModule, RouterModule],
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css'],
 })
